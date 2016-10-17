44613a5ee88036ce5277ea002000171957b2484a
vsavchuk <vsavchuk@datakernel.io>
2016-10-17 14:56:24 +0300

    RpcRemoveException extends SimpleException

--- rpc/src/main/java/io/datakernel/rpc/protocol/RpcRemoteException.java
+++ rpc/src/main/java/io/datakernel/rpc/protocol/RpcRemoteException.java
@@ -16,11 +16,12 @@
 
 package io.datakernel.rpc.protocol;
 
+import io.datakernel.exception.SimpleException;
 import io.datakernel.serializer.annotations.Deserialize;
 import io.datakernel.serializer.annotations.Serialize;
 import io.datakernel.serializer.annotations.SerializeNullable;
 
-public class RpcRemoteException extends Exception implements RpcMandatoryData {
+public class RpcRemoteException extends SimpleException implements RpcMandatoryData {
 	private static final long serialVersionUID = 769022174067373741L;
 	private final String causeMessage;
 	private final String causeClassName;
