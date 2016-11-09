6f2ea03ceb322d07082245278a3fd8cd8534298d
Vitaly Mykhalko <vmykhalko@datakernel.io>
2016-11-09 19:46:06 +0200

    store callback stack traces in CallbackRegistry

--- eventloop/src/main/java/io/datakernel/async/CallbackRegistry.java
+++ eventloop/src/main/java/io/datakernel/async/CallbackRegistry.java
@@ -25,40 +25,183 @@ import java.util.concurrent.ConcurrentHashMap;
 import static io.datakernel.jmx.MBeanFormat.formatDuration;
 
 public final class CallbackRegistry {
-	private static final ConcurrentHashMap<Object, Long> REGISTRY = new ConcurrentHashMap<>();
+	private static final ConcurrentHashMap<Object, CallbackMetaData> REGISTRY = new ConcurrentHashMap<>();
 
+	private static volatile boolean storeStackTrace = false;
+
+	// region public api
 	public static void register(Object callback) {
-		assert REGISTRY.put(callback, System.currentTimeMillis()) == null : "Callback " + callback + " has already been registered";
+		assert registerIfAssertionsEnabled(callback) : "Callback " + callback + " has already been registered";
+
 	}
 
 	public static void complete(Object callback) {
 		assert REGISTRY.remove(callback) != null : "Callback " + callback + " has already been completed";
 	}
 
+	public static boolean getStoreStackTrace() {
+		return storeStackTrace;
+	}
+
+	public static void setStoreStackTrace(boolean store) {
+		storeStackTrace = store;
+	}
+	// endregion
+
+	// region implementation
+	private static boolean registerIfAssertionsEnabled(Object callback) {
+		long timestamp = System.currentTimeMillis();
+
+		StackTraceElement[] stackTrace = null;
+		if (storeStackTrace) {
+			// TODO(vmykhalko): maybe use new Exception().getStackTrace instead ? according to performance issues
+			StackTraceElement[] fullStackTrace = Thread.currentThread().getStackTrace();
+			// remove stack trace lines that stand for registration method calls
+			stackTrace = Arrays.copyOfRange(fullStackTrace, 3, fullStackTrace.length);
+		}
+
+		return REGISTRY.put(callback, new CallbackMetaData(timestamp, stackTrace)) == null;
+	}
+
+	private static final class CallbackMetaData {
+		private final long timestamp;
+		private final StackTraceElement[] stackTrace;
+
+		public CallbackMetaData(long timestamp, StackTraceElement[] stackTrace) {
+			this.timestamp = timestamp;
+			this.stackTrace = stackTrace;
+		}
+
+		public long getTimestamp() {
+			return timestamp;
+		}
+
+		public StackTraceElement[] getStackTrace() {
+			return stackTrace;
+		}
+	}
+	// endregion
+
+	// region jmx
 	public static final class CallbackRegistryStats implements ConcurrentJmxMBean {
+		private volatile int maxCallbacksToShow = 100;
 
 		@JmxAttribute
-		public List<String> getCurrentCallbacks() {
-			List<Map.Entry<Object, Long>> entries = new ArrayList<>(REGISTRY.entrySet());
-			Collections.sort(entries, new Comparator<Map.Entry<Object, Long>>() {
-				@Override
-				public int compare(Map.Entry<Object, Long> o1, Map.Entry<Object, Long> o2) {
-					return o1.getValue().compareTo(o2.getValue());
-				}
-			});
+		public List<String> getCurrentCallbacksSummary() {
+			List<CallbackDetails> callbacksDetails = getCurrentCallbacksDetails();
 
 			List<String> lines = new ArrayList<>();
 			lines.add("Duration       Callback");
-			long currentTime = System.currentTimeMillis();
-			for (Map.Entry<Object, Long> entry : entries) {
-				String duration = formatDuration(currentTime - entry.getValue());
-				Object callback = entry.getKey();
-				String className = callback.getClass().getName();
-				String callbackString = callback.toString();
-				lines.add(String.format("%s   %s: %s", duration, className, callbackString));
+			for (CallbackDetails details : callbacksDetails) {
+				lines.add(String.format("%s   %s: %s",
+						details.getDuration(), details.getClassName(), details.getStringRepresentation()));
 			}
 
 			return lines;
 		}
+
+		@JmxAttribute
+		public List<CallbackDetails> getCurrentCallbacksDetails() {
+			List<Map.Entry<Object, CallbackMetaData>> sortedEntries = sortByTimestamp(REGISTRY);
+
+			List<CallbackDetails> detailsList = new ArrayList<>();
+			long currentTime = System.currentTimeMillis();
+			int maxCallbacksToShowCached = maxCallbacksToShow;
+			for (Map.Entry<Object, CallbackMetaData> entry : sortedEntries) {
+				if (detailsList.size() == maxCallbacksToShowCached) {
+					break;
+				}
+
+				Object callback = entry.getKey();
+				CallbackMetaData metaData = entry.getValue();
+				String formattedDuration = formatDuration(currentTime - metaData.getTimestamp());
+				String className = callback.getClass().getName();
+				String callbackString = callback.toString();
+
+				List<String> stackTraceLines = new ArrayList<>();
+				StackTraceElement[] stackTrace = metaData.getStackTrace();
+				if (stackTrace != null) {
+					for (StackTraceElement stackTraceElement : stackTrace) {
+						stackTraceLines.add(stackTraceElement.toString());
+					}
+				}
+
+				detailsList.add(new CallbackDetails(className, callbackString, formattedDuration, stackTraceLines));
+			}
+
+			return detailsList;
+		}
+
+		@JmxAttribute
+		public boolean getStoreStackTrace() {
+			return CallbackRegistry.getStoreStackTrace();
+		}
+
+		@JmxAttribute
+		public void setStoreStackTrace(boolean store) {
+			CallbackRegistry.setStoreStackTrace(store);
+		}
+
+		@JmxAttribute
+		public int getMaxCallbacksToShow() {
+			return maxCallbacksToShow;
+		}
+
+		@JmxAttribute
+		public void setMaxCallbacksToShow(int maxCallbacksToShow) {
+			if (maxCallbacksToShow < 0) {
+				throw new IllegalArgumentException("argument must be non-negative");
+			}
+			this.maxCallbacksToShow = maxCallbacksToShow;
+		}
+
+		private static List<Map.Entry<Object, CallbackMetaData>> sortByTimestamp(Map<Object, CallbackMetaData> map) {
+			List<Map.Entry<Object, CallbackMetaData>> sortedEntries = new ArrayList<>(map.entrySet());
+			Collections.sort(sortedEntries, new Comparator<Map.Entry<Object, CallbackMetaData>>() {
+				@Override
+				public int compare(Map.Entry<Object, CallbackMetaData> o1, Map.Entry<Object, CallbackMetaData> o2) {
+					long ts1 = o1.getValue().getTimestamp();
+					long ts2 = o2.getValue().getTimestamp();
+					return Long.compare(ts1, ts2);
+				}
+			});
+			return sortedEntries;
+		}
+
+		public static final class CallbackDetails {
+			private final String className;
+			private final String stringRepresentation;
+			private final String duration;
+			private final List<String> stackTrace;
+
+			public CallbackDetails(String className, String stringRepresentation,
+			                       String duration, List<String> stackTrace) {
+				this.className = className;
+				this.stringRepresentation = stringRepresentation;
+				this.duration = duration;
+				this.stackTrace = stackTrace;
+			}
+
+			@JmxAttribute
+			public String getClassName() {
+				return className;
+			}
+
+			@JmxAttribute
+			public String getStringRepresentation() {
+				return stringRepresentation;
+			}
+
+			@JmxAttribute
+			public String getDuration() {
+				return duration;
+			}
+
+			@JmxAttribute
+			public List<String> getStackTrace() {
+				return stackTrace;
+			}
+		}
 	}
+	// endregion
 }
