520f846af26b6ab7d60c96ecdfaeb27139067f79
Eduard Vasinskyi <evasinskyi@datakernel.io>
2018-07-20 14:30:10 +0300

    Add ability to serialize/deserialize enum as varInt

--- serializer/src/main/java/io/datakernel/serializer/asm/SerializerGenEnum.java
+++ serializer/src/main/java/io/datakernel/serializer/asm/SerializerGenEnum.java
@@ -28,17 +28,16 @@ import static io.datakernel.codegen.Expressions.*;
 
 @SuppressWarnings("StatementWithEmptyBody")
 public class SerializerGenEnum implements SerializerGen, NullableOptimization {
-	private final Class<?> nameOfEnum;
+	private final Class<?> enumType;
 	private final boolean nullable;
 
-	public SerializerGenEnum(Class<?> c) {
-		this.nameOfEnum = c;
-		this.nullable = false;
+	public SerializerGenEnum(Class<?> enumType, boolean nullable) {
+		this.enumType = enumType;
+		this.nullable = nullable;
 	}
 
-	public SerializerGenEnum(Class<?> c, boolean nullable) {
-		this.nameOfEnum = c;
-		this.nullable = nullable;
+	public SerializerGenEnum(Class<?> enumType) {
+		this(enumType, false);
 	}
 
 	@Override
@@ -52,7 +51,7 @@ public class SerializerGenEnum implements SerializerGen, NullableOptimization {
 
 	@Override
 	public Class<?> getRawType() {
-		return nameOfEnum;
+		return enumType;
 	}
 
 	@Override
@@ -62,32 +61,54 @@ public class SerializerGenEnum implements SerializerGen, NullableOptimization {
 
 	@Override
 	public Expression serialize(Expression byteArray, Variable off, Expression value, int version, SerializerBuilder.StaticMethods staticMethods, CompatibilityLevel compatibilityLevel) {
-		Expression ordinal = cast(call(cast(value, Enum.class), "ordinal"), Type.BYTE_TYPE);
-		if (!nullable) {
-			return callStatic(SerializationUtils.class, "writeByte", byteArray, off, ordinal);
-		} else {
-			return ifThenElse(isNull(value),
-					callStatic(SerializationUtils.class, "writeByte", byteArray, off, value((byte) 0)),
-					callStatic(SerializationUtils.class, "writeByte", byteArray, off, cast(add(ordinal, value((byte) 1)), Type.BYTE_TYPE))
-			);
+		Expression ordinal = call(cast(value, Enum.class), "ordinal");
+		if (isSmallEnum()) {
+			ordinal = cast(ordinal, Type.BYTE_TYPE);
+			return !nullable ?
+					callStatic(SerializationUtils.class, "writeByte", byteArray, off, ordinal) :
+					ifThenElse(isNull(value),
+							callStatic(SerializationUtils.class, "writeByte", byteArray, off, value((byte) 0)),
+							callStatic(SerializationUtils.class, "writeByte", byteArray, off, cast(add(ordinal, value((byte) 1)), Type.BYTE_TYPE)));
 		}
+		return !nullable ?
+				callStatic(SerializationUtils.class, "writeVarInt", byteArray, off, ordinal) :
+				ifThenElse(isNull(value),
+						callStatic(SerializationUtils.class, "writeVarInt", byteArray, off, value(0)),
+						callStatic(SerializationUtils.class, "writeVarInt", byteArray, off, inc(ordinal)));
 	}
 
 	@Override
-	public void prepareDeserializeStaticMethods(int version, SerializerBuilder.StaticMethods staticMethods, CompatibilityLevel compatibilityLevel) {
+	public void prepareDeserializeStaticMethods(int version, SerializerBuilder.
+			StaticMethods staticMethods, CompatibilityLevel compatibilityLevel) {
 
 	}
 
 	@Override
-	public Expression deserialize(Class<?> targetType, int version, SerializerBuilder.StaticMethods staticMethods, CompatibilityLevel compatibilityLevel) {
-		Variable value = let(call(arg(0), "readByte"));
-		if (!nullable) {
-			return getArrayItem(callStatic(nameOfEnum, "values"), value);
-		} else {
-			return ifThenElse(cmpEq(value, value((byte) 0)),
-					nullRef(nameOfEnum),
-					getArrayItem(callStatic(nameOfEnum, "values"), sub(value, value(1))));
+	public Expression deserialize(Class<?> targetType, int version, SerializerBuilder.
+			StaticMethods staticMethods, CompatibilityLevel compatibilityLevel) {
+		if (isSmallEnum()) {
+			Variable value = let(call(arg(0), "readByte"));
+			return !nullable ?
+					getArrayItem(callStatic(enumType, "values"), value) :
+					ifThenElse(cmpEq(value, value((byte) 0)),
+							nullRef(enumType),
+							getArrayItem(callStatic(enumType, "values"), dec(value)));
 		}
+		Variable value = let(call(arg(0), "readVarInt"));
+		return !nullable ?
+				getArrayItem(callStatic(enumType, "values"), value) :
+				ifThenElse(cmpEq(value, value(0)),
+						nullRef(enumType),
+						getArrayItem(callStatic(enumType, "values"), dec(value)));
+	}
+
+	private boolean isSmallEnum() {
+		return enumType.getEnumConstants().length <= Byte.MAX_VALUE - (nullable ? 1 : 0);
+	}
+
+	@Override
+	public SerializerGen setNullable() {
+		return new SerializerGenEnum(enumType, true);
 	}
 
 	@Override
@@ -97,20 +118,11 @@ public class SerializerGenEnum implements SerializerGen, NullableOptimization {
 
 		SerializerGenEnum that = (SerializerGenEnum) o;
 
-		if (nullable != that.nullable) return false;
-		return !(nameOfEnum != null ? !nameOfEnum.equals(that.nameOfEnum) : that.nameOfEnum != null);
-
+		return nullable == that.nullable && enumType.equals(that.enumType);
 	}
 
 	@Override
 	public int hashCode() {
-		int result = nameOfEnum != null ? nameOfEnum.hashCode() : 0;
-		result = 31 * result + (nullable ? 1 : 0);
-		return result;
-	}
-
-	@Override
-	public SerializerGen setNullable() {
-		return new SerializerGenEnum(nameOfEnum, true);
+		return 31 * enumType.hashCode() + (nullable ? 1 : 0);
 	}
 }
