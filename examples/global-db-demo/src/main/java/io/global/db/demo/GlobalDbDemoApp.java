4e0663da00b0cfe681b6851498be7e7802db42cb
Eduard Vasinskyi <evasinskyi@datakernel.io>
2019-04-08 12:48:30 +0300

    Rework global db demo app to use local node

--- examples/global-db-demo/src/main/java/io/global/db/demo/GlobalDbDemoApp.java
+++ examples/global-db-demo/src/main/java/io/global/db/demo/GlobalDbDemoApp.java
@@ -26,40 +26,25 @@ import io.datakernel.config.ConfigModule;
 import io.datakernel.csp.ChannelConsumer;
 import io.datakernel.csp.ChannelSupplier;
 import io.datakernel.eventloop.Eventloop;
-import io.datakernel.eventloop.ThrottlingController;
-import io.datakernel.http.AsyncHttpClient;
-import io.datakernel.http.IAsyncHttpClient;
 import io.datakernel.jmx.JmxModule;
 import io.datakernel.launcher.Launcher;
 import io.datakernel.service.ServiceGraphModule;
-import io.datakernel.util.guice.OptionalDependency;
-import io.global.common.BinaryDataFormats;
+import io.global.common.ExampleCommonModule;
 import io.global.common.KeyPair;
-import io.global.common.SignedData;
-import io.global.common.api.AnnounceData;
-import io.global.common.api.DiscoveryService;
-import io.global.common.discovery.HttpDiscoveryService;
 import io.global.db.DbItem;
 import io.global.db.GlobalDbDriver;
+import io.global.db.GlobalDbNodeImpl;
 import io.global.db.api.DbClient;
-import io.global.db.api.DbStorage;
-import io.global.db.api.GlobalDbNode;
-import io.global.db.http.HttpGlobalDbNode;
-import io.global.db.stub.RuntimeDbStorageStub;
+import io.global.launchers.GlobalNodesModule;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
 import java.util.function.Consumer;
 
+import static com.google.inject.util.Modules.override;
 import static io.datakernel.config.Config.ofProperties;
-import static io.datakernel.config.ConfigConverters.getExecutor;
-import static io.datakernel.config.ConfigConverters.ofInetSocketAddress;
-import static io.datakernel.launchers.initializers.Initializers.ofEventloop;
-import static io.datakernel.util.CollectionUtils.set;
 import static io.global.launchers.GlobalConfigConverters.ofPrivKey;
-import static io.global.launchers.GlobalConfigConverters.ofRawServerId;
 import static java.lang.Boolean.parseBoolean;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Arrays.asList;
@@ -67,6 +52,7 @@ import static java.util.Arrays.asList;
 public final class GlobalDbDemoApp extends Launcher {
 	public static final String EAGER_SINGLETONS_MODE = "eagerSingletonsMode";
 	public static final String PROPERTIES_FILE = "globaldb-app.properties";
+	public static final String DEFAULT_SERVER_ID = "DB Demo";
 
 	public static final Consumer<DbItem> DB_ITEM_CONSUMER = dbItem -> {
 		System.out.print("Key: " + new String(dbItem.getKey(), UTF_8));
@@ -80,67 +66,20 @@ public final class GlobalDbDemoApp extends Launcher {
 	@Named("alice")
 	DbClient alice;
 
-	@Inject
-	@Named("alice")
-	KeyPair aliceKeys;
-
-	@Inject
-	DiscoveryService discoveryService;
-
-	@Inject
-	SignedData<AnnounceData> announceData;
-
 	@Override
 	protected Collection<com.google.inject.Module> getModules() {
 		return asList(ServiceGraphModule.defaultInstance(),
 				JmxModule.create(),
 				ConfigModule.create(() ->
 						Config.create()
+								.with("node.serverId", DEFAULT_SERVER_ID)
 								.override(ofProperties(PROPERTIES_FILE, true))
 								.override(ofProperties(System.getProperties()).getChild("config")))
 						.printEffectiveConfig(),
-				new AbstractModule() {
+				override(new AbstractModule() {
 					@Provides
 					@Singleton
-					Eventloop provide(Config config, OptionalDependency<ThrottlingController> maybeThrottlingController) {
-						return Eventloop.create()
-								.initialize(ofEventloop(config.getChild("eventloop")))
-								.initialize(eventloop -> maybeThrottlingController.ifPresent(eventloop::withInspector));
-					}
-
-					@Provides
-					@Singleton
-					ExecutorService provide(Config config) {
-						return getExecutor(config);
-					}
-
-					@Provides
-					@Singleton
-					IAsyncHttpClient provide(Eventloop eventloop) {
-						return AsyncHttpClient.create(eventloop);
-					}
-
-					@Provides
-					@Singleton
-					DiscoveryService provideDiscovery(IAsyncHttpClient httpClient, Config config) {
-						return HttpDiscoveryService.create(config.get(ofInetSocketAddress(), "app.discoveryAddress"), httpClient);
-					}
-
-					@Provides
-					@Singleton
-					DbStorage provideStorage() {
-						return new RuntimeDbStorageStub();
-					}
-
-					@Provides
-					@Singleton
-					GlobalDbNode provide(IAsyncHttpClient httpClient, Config config) {
-						return HttpGlobalDbNode.create(config.get("app.globalDbId"), httpClient);
-					}
-
-					@Provides
-					@Singleton
-					GlobalDbDriver provide(GlobalDbNode node) {
+					GlobalDbDriver provide(GlobalDbNodeImpl node) {
 						return GlobalDbDriver.create(node);
 					}
 
@@ -157,14 +96,8 @@ public final class GlobalDbDemoApp extends Launcher {
 					DbClient provideAlice(GlobalDbDriver driver, @Named("alice") KeyPair keys) {
 						return driver.adapt(keys);
 					}
-
-					@Provides
-					@Singleton
-					SignedData<AnnounceData> provideAnnounceData(Config config) {
-						return SignedData.sign(BinaryDataFormats.REGISTRY.get(AnnounceData.class), AnnounceData.of(System.currentTimeMillis(),
-								set(config.get(ofRawServerId(), "app.globalDbId"))), aliceKeys.getPrivKey());
-					}
-				});
+				}, new GlobalNodesModule())
+						.with(new ExampleCommonModule()));
 	}
 
 	@Override
@@ -176,8 +109,7 @@ public final class GlobalDbDemoApp extends Launcher {
 		}
 
 		eventloop.post(() ->
-				discoveryService.announce(aliceKeys.getPubKey(), announceData)
-						.then($ -> alice.upload("test_table"))
+				alice.upload("test_table")
 						.then(ChannelSupplier.ofIterable(dbItems)::streamTo)
 						.whenException(Throwable::printStackTrace)
 						.whenResult($ -> System.out.println("Data items have been uploaded to database\nDownloading back..."))
