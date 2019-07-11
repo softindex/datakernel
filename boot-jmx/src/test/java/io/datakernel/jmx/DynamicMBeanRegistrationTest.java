b4131718a96563be6896e24df7fd26768f1b79d6
Dmitry Volvach <dmitry@datakernel.io>
2019-07-11 21:45:06 +0300

    Extract Boot:JMX module from Boot module, with optional Boot:JMX-API module

--- boot-jmx/src/test/java/io/datakernel/jmx/DynamicMBeanRegistrationTest.java
+++ boot-jmx/src/test/java/io/datakernel/jmx/DynamicMBeanRegistrationTest.java
@@ -16,8 +16,8 @@
 
 package io.datakernel.jmx;
 
-import io.datakernel.di.core.Key;
 import io.datakernel.di.annotation.NameAnnotation;
+import io.datakernel.di.core.Key;
 import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
--- boot-servicegraph/src/test/java/io/datakernel/test/DatakernelServiceRunnerTest.java
+++ boot-servicegraph/src/test/java/io/datakernel/test/DatakernelServiceRunnerTest.java
@@ -8,13 +8,12 @@ import io.datakernel.service.ServiceGraphModule;
 import io.datakernel.test.DatakernelRunner.UseModules;
 import io.datakernel.test.DatakernelServiceRunnerTest.TestModule;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.concurrent.CompletableFuture;
-
-import static org.junit.Assert.*;
 
 @RunWith(DatakernelServiceRunner.class)
 @UseModules({TestModule.class})
@@ -56,37 +55,37 @@ public class DatakernelServiceRunnerTest {
 	@Before
 	public void before(Service service) {
 		serviceCopy = service;
-		assertTrue(service instanceof TestService);
-		assertFalse(((TestService) service).starting);
-		assertFalse(((TestService) service).stopped);
+		Assert.assertTrue(service instanceof TestService);
+		Assert.assertFalse(((TestService) service).starting);
+		Assert.assertFalse(((TestService) service).stopped);
 	}
 
 	@Test
 	public void test(Service service, @Named("SecondInstance") Service second) {
-		assertSame(service, serviceCopy);
-		assertNotSame(service, second);
-		
-		assertTrue(service instanceof TestService);
-		assertTrue(((TestService) service).starting);
-		assertFalse(((TestService) service).stopped);
+		Assert.assertSame(service, serviceCopy);
+		Assert.assertNotSame(service, second);
 
-		assertTrue(second instanceof TestService);
-		assertTrue(((TestService) second).starting);
-		assertFalse(((TestService) second).stopped);
+		Assert.assertTrue(service instanceof TestService);
+		Assert.assertTrue(((TestService) service).starting);
+		Assert.assertFalse(((TestService) service).stopped);
+
+		Assert.assertTrue(second instanceof TestService);
+		Assert.assertTrue(((TestService) second).starting);
+		Assert.assertFalse(((TestService) second).stopped);
 	}
 
 	@After
 	public void after(Service service) {
-		assertTrue(service instanceof TestService);
-		assertTrue(((TestService) service).starting);
-		assertTrue(((TestService) service).stopped);
+		Assert.assertTrue(service instanceof TestService);
+		Assert.assertTrue(((TestService) service).starting);
+		Assert.assertTrue(((TestService) service).stopped);
 	}
 
 	@RunWith(DatakernelServiceRunner.class)
 	public static class WithoutModulesTest {
 		@Test
 		public void test() {
-			assertTrue(true);
+			Assert.assertTrue(true);
 		}
 	}
 
@@ -97,18 +96,18 @@ public class DatakernelServiceRunnerTest {
 		@Before
 		@UseModules({TestModule.class})
 		public void before(Service service) {
-			assertTrue(service instanceof TestService);
-			assertFalse(((TestService) service).starting);
-			assertFalse(((TestService) service).stopped);
+			Assert.assertTrue(service instanceof TestService);
+			Assert.assertFalse(((TestService) service).starting);
+			Assert.assertFalse(((TestService) service).stopped);
 			testService = service;
 		}
 
 		@Test
 		public void test() {
-			assertNotNull(testService);
-			assertTrue(testService instanceof TestService);
-			assertTrue(((TestService) testService).starting);
-			assertFalse(((TestService) testService).stopped);
+			Assert.assertNotNull(testService);
+			Assert.assertTrue(testService instanceof TestService);
+			Assert.assertTrue(((TestService) testService).starting);
+			Assert.assertFalse(((TestService) testService).stopped);
 		}
 	}
 }
