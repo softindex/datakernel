f0087e11583428fa9f1d7899affdc0d5ff7668d2
Dmitry Volvach <dmitry@datakernel.io>
2019-09-27 21:39:42 +0300

    Simplify archetype-http servlet

--- archetypes/http/src/main/resources/archetype-resources/src/main/java/__mainClassName__.java
+++ archetypes/http/src/main/resources/archetype-resources/src/main/java/__mainClassName__.java
@@ -7,17 +7,13 @@ import io.datakernel.http.HttpResponse;
 import io.datakernel.launcher.Launcher;
 import io.datakernel.launchers.http.HttpServerLauncher;
 
-import java.time.LocalDateTime;
-
-import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-
 public class ${mainClassName} extends HttpServerLauncher {
 
     @Provides
     AsyncServlet servlet() {
         return request -> Promise.of(
                 HttpResponse.ok200()
-                    .withPlainText("Hello World at " + LocalDateTime.now().format(ISO_LOCAL_DATE_TIME)));
+                    .withPlainText("Hello, World!"));
     }
 
     public static void main(String[] args) throws Exception {
