164be5dc8e4e1e78f186c94bea748a2141a85caf
Anton Bulakh <abulah@datakernel.io>
2019-12-19 17:21:07 +0200

    Add router module which automatically maps certain servlets provided via DI

---
+++ examples/core/http/src/main/java/RouterModuleExample.java
@@ -0,0 +1,47 @@
+import io.datakernel.di.annotation.Provides;
+import io.datakernel.di.module.Module;
+import io.datakernel.di.module.Modules;
+import io.datakernel.http.AsyncServlet;
+import io.datakernel.http.HttpResponse;
+import io.datakernel.http.di.RouterModule;
+import io.datakernel.http.di.RouterModule.Mapped;
+import io.datakernel.http.di.RouterModule.Router;
+import io.datakernel.launchers.http.HttpServerLauncher;
+
+//[START EXAMPLE]
+public final class RouterModuleExample extends HttpServerLauncher {
+
+	@Override
+	protected Module getBusinessLogicModule() {
+		return Modules.combine(new RouterModule());
+	}
+
+	@Provides
+	AsyncServlet servlet(@Router AsyncServlet router) {
+		return router;
+	}
+
+	@Provides
+	@Mapped("/")
+	AsyncServlet main() {
+		return request -> HttpResponse.ok200().withPlainText("hello world");
+	}
+
+	@Provides
+	@Mapped("/test1")
+	AsyncServlet test1() {
+		return request -> HttpResponse.ok200().withPlainText("this is test 1");
+	}
+
+	@Provides
+	@Mapped("/*")
+	AsyncServlet others() {
+		return request -> HttpResponse.ok200().withPlainText("this is the fallback: " + request.getRelativePath());
+	}
+
+	public static void main(String[] args) throws Exception {
+		RouterModuleExample example = new RouterModuleExample();
+		example.launch(args);
+	}
+}
+//[END EXAMPLE]
