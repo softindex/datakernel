ae5aa62849c756eb46e9ea904499ee4a41326de2
Eduard Vasinskyi <evasinskyi@datakernel.io>
2020-02-17 19:06:31 +0200

    Add test to demonstrate context wrapping for Promise pretty printing

--- core-promise/src/test/java/io/datakernel/promise/AbstractPromiseTest.java
+++ core-promise/src/test/java/io/datakernel/promise/AbstractPromiseTest.java
@@ -1,10 +1,14 @@
 package io.datakernel.promise;
 
+import io.datakernel.async.callback.Callback;
+import io.datakernel.common.ref.Ref;
 import io.datakernel.common.ref.RefInt;
+import io.datakernel.eventloop.Eventloop;
 import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 public final class AbstractPromiseTest {
 
@@ -37,5 +41,38 @@ public final class AbstractPromiseTest {
 
 		assertEquals("12345", sb.toString());
 
+	}
+
+	@Test
+	public void toAnotherEventloop() throws InterruptedException {
+		System.setProperty("RunnableWithContext.wrapContext", "true");
+
+		Ref<Object> contextRef = new Ref<>();
+		Eventloop currentEventloop = Eventloop.create().withCurrentThread().withFatalErrorHandler((e, context) -> contextRef.set(context));
+		Eventloop anotherEventloop = Eventloop.create();
+
+		anotherEventloop.startExternalTask();
+		Thread thread = new Thread(anotherEventloop);
+
+		SettablePromise<Integer> cb = new SettablePromise<>();
+		cb.map($ -> $)
+				.then(Promise::of)
+				.whenResult($ -> {})
+				.whenComplete(() -> {throw new RuntimeException();});
+
+		currentEventloop.post(() -> {
+			Callback<Integer> anotherEventloopCb = Callback.toAnotherEventloop(currentEventloop, cb);
+			anotherEventloop.completeExternalTask();
+			anotherEventloopCb.accept(123, null);
+		});
+
+		thread.start();
+		currentEventloop.run();
+		thread.join();
+
+		Object caughtContext = contextRef.get();
+		assertSame(cb, caughtContext);
+
+		System.out.println(caughtContext);
 	}
 }
