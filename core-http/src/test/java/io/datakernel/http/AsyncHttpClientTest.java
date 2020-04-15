deb215d87502617d1abde58a740e9e07f964abfa
Eduard Vasinskyi <evasinskyi@datakernel.io>
2020-04-15 18:20:08 +0300

    Fix bug with negative 'activeRequests' JMX attribute

--- core-http/src/test/java/io/datakernel/http/AsyncHttpClientTest.java
+++ core-http/src/test/java/io/datakernel/http/AsyncHttpClientTest.java
@@ -192,6 +192,27 @@ public final class AsyncHttpClientTest {
 	}
 
 	@Test
+	public void testActiveRequestsCounterWithoutRefresh() throws IOException {
+		Eventloop eventloop = Eventloop.getCurrentEventloop();
+
+		AsyncHttpServer server = AsyncHttpServer.create(eventloop,
+				request -> HttpResponse.ok200())
+				.withAcceptOnce()
+				.withListenPort(PORT);
+
+		server.listen();
+
+		JmxInspector inspector = new JmxInspector();
+		AsyncHttpClient httpClient = AsyncHttpClient.create(eventloop)
+				.withInspector(inspector);
+
+		Promise<HttpResponse> requestPromise = httpClient.request(HttpRequest.get("http://127.0.0.1:" + PORT));
+		assertEquals(1, inspector.getActiveRequests());
+		await(requestPromise);
+		assertEquals(0, inspector.getActiveRequests());
+	}
+
+	@Test
 	public void testClientNoContentLength() throws Exception {
 		String text = "content";
 		ByteBuf req = ByteBuf.wrapForReading(encodeAscii("HTTP/1.1 200 OK\r\n\r\n" + text));
@@ -301,13 +322,11 @@ public final class AsyncHttpClientTest {
 	};
 
 	private Promise<HttpResponse> customResponse(ByteBuf rawResponse, boolean ssl) throws IOException {
-		SimpleServer server = SimpleServer.create(asyncTcpSocket -> {
-			BinaryChannelSupplier.of(ChannelSupplier.ofSocket(asyncTcpSocket))
-					.parse(REQUEST_PARSER)
-					.whenResult(ByteBuf::recycle)
-					.then($ -> asyncTcpSocket.write(rawResponse))
-					.whenResult($ -> asyncTcpSocket.close());
-		})
+		SimpleServer server = SimpleServer.create(asyncTcpSocket -> BinaryChannelSupplier.of(ChannelSupplier.ofSocket(asyncTcpSocket))
+				.parse(REQUEST_PARSER)
+				.whenResult(ByteBuf::recycle)
+				.then($ -> asyncTcpSocket.write(rawResponse))
+				.whenResult($ -> asyncTcpSocket.close()))
 				.withAcceptOnce();
 		if (ssl) {
 			server.withSslListenAddress(createSslContext(), Executors.newSingleThreadExecutor(), new InetSocketAddress(PORT));
